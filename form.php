30eb1950c29b96a665392fea9893c4cb0dc12c61
Scott Wren <cynics0xff@gmail.com>
2019-07-09 15:17:31 -0400

    Add gender drop down

--- form.php
+++ form.php
@@ -49,12 +49,12 @@
                                 </button>
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                     <a class="dropdown-item" href="#">Female</a>
-                                    <a class="dropdown-item" href="#">Male</a>
+                                    <a class="dropdown-item" href="#" value="M" selected>Male</a></li></a>
                                 </div>
                             </div>
-
+                            <br>
                             <div class="form-group">
-                                <label for="datepicker">SSN</label>
+                                <label for="datepicker">DOB</label>
                                 <input id="datepicker" width="276" value="1974-01-20" />
                                 <script>
                                     $('#datepicker').datepicker({
@@ -63,7 +63,6 @@
                                 </script>
                                 <div class="invalid-feedback">Select a dob.</div>
                             </div>
-
                             <div class="form-group">
                                 <label for="ssn">SSN</label>
                                 <input type="text" class="form-control form-control-lg rounded-0" name="ssn" id="ssn" required="" value="111-22-3333">
@@ -83,7 +82,7 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="city">City</label>
+                                <label for="state">State</label>
                                 <input type="text" class="form-control form-control-lg rounded-0" name="state" id="state" required="" value="PA">
                                 <div class="invalid-feedback">Enter the state.</div>
                             </div>
@@ -92,6 +91,65 @@
                                 <label for="zip">Zip</label>
                                 <input type="text" class="form-control form-control-lg rounded-0" name="zip" id="zip" required="" value="18505">
                                 <div class="invalid-feedback">Enter the address.</div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="state" class="control-label">State</label>
+                                    <select class="form-control" id="state" name="state">
+                                        <option value="">N/A</option>
+                                        <option value="AK">Alaska</option>
+                                        <option value="AL">Alabama</option>
+                                        <option value="AR">Arkansas</option>
+                                        <option value="AZ">Arizona</option>
+                                        <option value="CA">California</option>
+                                        <option value="CO">Colorado</option>
+                                        <option value="CT">Connecticut</option>
+                                        <option value="DC">District of Columbia</option>
+                                        <option value="DE">Delaware</option>
+                                        <option value="FL">Florida</option>
+                                        <option value="GA">Georgia</option>
+                                        <option value="HI">Hawaii</option>
+                                        <option value="IA">Iowa</option>
+                                        <option value="ID">Idaho</option>
+                                        <option value="IL">Illinois</option>
+                                        <option value="IN">Indiana</option>
+                                        <option value="KS">Kansas</option>
+                                        <option value="KY">Kentucky</option>
+                                        <option value="LA">Louisiana</option>
+                                        <option value="MA">Massachusetts</option>
+                                        <option value="MD">Maryland</option>
+                                        <option value="ME">Maine</option>
+                                        <option value="MI">Michigan</option>
+                                        <option value="MN">Minnesota</option>
+                                        <option value="MO">Missouri</option>
+                                        <option value="MS">Mississippi</option>
+                                        <option value="MT">Montana</option>
+                                        <option value="NC">North Carolina</option>
+                                        <option value="ND">North Dakota</option>
+                                        <option value="NE">Nebraska</option>
+                                        <option value="NH">New Hampshire</option>
+                                        <option value="NJ">New Jersey</option>
+                                        <option value="NM">New Mexico</option>
+                                        <option value="NV">Nevada</option>
+                                        <option value="NY">New York</option>
+                                        <option value="OH">Ohio</option>
+                                        <option value="OK">Oklahoma</option>
+                                        <option value="OR">Oregon</option>
+                                        <option value="PA" selected>Pennsylvania</option>
+                                        <option value="PR">Puerto Rico</option>
+                                        <option value="RI">Rhode Island</option>
+                                        <option value="SC">South Carolina</option>
+                                        <option value="SD">South Dakota</option>
+                                        <option value="TN">Tennessee</option>
+                                        <option value="TX">Texas</option>
+                                        <option value="UT">Utah</option>
+                                        <option value="VA">Virginia</option>
+                                        <option value="VT">Vermont</option>
+                                        <option value="WA">Washington</option>
+                                        <option value="WI">Wisconsin</option>
+                                        <option value="WV">West Virginia</option>
+                                        <option value="WY">Wyoming</option>
+                                    </select>
                             </div>
 
                             <div class="form-group">
